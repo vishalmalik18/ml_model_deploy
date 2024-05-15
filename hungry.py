6ec7ab07d25d79fd4c30fd0a7f5d26d09103c9a9
vishal <vishalmalik02003@gmail.com>
2024-05-15 16:04:42 +0530

    add samosa pizza burger and fries

--- hungry.py
+++ hungry.py
@@ -1,6 +1,8 @@
 hungry = input("are you hungry?")
 if hungry=='Yes':
     print("eat samosa")
-
+    print('eat pizza')
+    print('eat burger')
+    print('eat fries')
 else:
     print("do you homework")
