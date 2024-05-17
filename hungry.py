86a44f8f91d150745722bf6afdc6ca04649723af
vishal <vishalmalik02003@gmail.com>
2024-05-17 18:56:36 +0530

    updated_code

--- hungry.py
+++ hungry.py
@@ -1,10 +1,5 @@
 hungry = input("are you hungry?")
 if hungry=='Yes':
     print("eat samosa")
-    print('eat pizza')
-    print('eat burger')
-    print('eat fries')
-
-
 else:
     print('do your homework')
